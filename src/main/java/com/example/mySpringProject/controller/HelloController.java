8218a6a0b157d2800bf7996a19de97c1586419e4
Michael Chan <michaelcychan@gmail.com>
2023-03-02 21:55:23 +0000

    add controller and simple mapping with params ✍️

--- src/main/java/com/example/mySpringProject/controller/HelloController.java
+++ src/main/java/com/example/mySpringProject/controller/HelloController.java
@@ -1,15 +1,30 @@
 package com.example.mySpringProject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
+/*
+* the @ symbol corresponds to the annotation.
+* The @GetMapping and @RestController are examples of Annotations.
+* Annotations are built-in units of Spring that are readily available.
+* @GetMapping will be run every time it receives a request to the endpoint
+ */
 
-// the @ symbol corresponds to the annotation.
-// The @GetMapping and @RestController are examples of Annotations.
+// RequestMapping can be used for the whole controller or individual methods
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
-    @GetMapping("/helloworld")
+    // @GetMapping() empty because of the class has a mapping
+    @GetMapping()
     public String helloWorld() {
         return "Hello world!";
+    }
+
+    // this extra mapping makes sub-domain of /hello => /hello/{name}
+    @GetMapping("/{name}")
+    public String helloName(@PathVariable String name) {
+        return "Hello, " + name + "!";
     }
 }
